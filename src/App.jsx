2c0415db0c9fc0a6c8be831d5414dcd396b58410
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-10-13 20:45:42 -0700

    add header & styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Header from './components/header';
 import ImageSlider from './components/image-slider';
 import { IMAGES } from './components/images'; 
 
 const App = () => (
-  <div style={{ maxWidth: "500px", width: '100%', aspectRatio: '1', margin: "0 auto" }}>
-    <ImageSlider images={IMAGES} />
+  <div style={{ backgroundColor: 'orange' }}>
+    <Header />
+    <section style={{ maxWidth: "600px", width: '100%', aspectRatio: '1', margin: "0 auto" }}>
+      <ImageSlider images={IMAGES} />
+    </section>
   </div>
 );
 
