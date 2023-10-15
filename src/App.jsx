ff864b97bfdcbd5539ea6963f13dc7e17b378d02
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-10-14 20:10:49 -0700

    background image added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Header from './components/header';
 import ImageSlider from './components/image-slider';
 import { IMAGES } from './components/images'; 
 
+import './components/background.css';
+
 const App = () => (
-  <div style={{ backgroundColor: 'orange' }}>
+  <div className="background">
     <Header />
-    <section style={{ maxWidth: "600px", width: '100%', aspectRatio: '1', margin: "0 auto" }}>
+    {/* <section style={{ maxWidth: "600px", width: '100%', aspectRatio: '1', margin: "0 auto" }}>
       <ImageSlider images={IMAGES} />
-    </section>
+    </section> */}
   </div>
 );
 
