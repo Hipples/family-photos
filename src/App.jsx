dc65ae1e5d963213a5aca17b7d65da29a4b75a8d
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-10-15 12:10:33 -0700

    refactored hanging sign component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import Header from './components/header';
 import ImageSlider from './components/image-slider';
 import { IMAGES } from './components/images'; 
 
-import sign from './assets/hanging-sign.png';
 import './background.css';
+import HangingSign from './components/hanging-sign';
+
+const albumDetails = {
+  title: "A Day at the Vancouver Pumpkin Patch",
+  date: "October 8, 2023"
+}
 
 const App = () => (
   <div className="background">
-    <div className="sign-container">
-      <img className="sign" src={sign} />
-      <div className="sign-text">
-          <h1>A Day at the<br/>Vancouver<br/>Pumpkin Patch</h1></div>
-      <div className="sign-date">October 8, 2023</div>
-    </div>
-    {/* <Header /> */}
+    <HangingSign albumDetails={albumDetails}/>
     {/* <section style={{ maxWidth: "600px", width: '100%', aspectRatio: '1', margin: "0 auto" }}>
       <ImageSlider images={IMAGES} />
     </section> */}
